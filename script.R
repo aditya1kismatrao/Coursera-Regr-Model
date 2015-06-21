cc463c9956d96895f8c23586d210d56fd993e856
pskotarczak <pskotarczak@gmail.com>
2015-06-21 18:40:27 +0200

    script update and first report.rmd commit

--- script.R
+++ script.R
@@ -10,10 +10,14 @@ head(mtcars)
 plot(mtcars$mpg, factor(mtcars$am))
 
 # gathering mpg for automatic transmission
-auto <- mtcars$mpg[mtcars$am == 1]
+auto <- mtcars$mpg[mtcars$am == 0]
 
 #gathering mpg for manual transmission
-manu <- mtcars$mpg[mtcars$am == 0]
+manu <- mtcars$mpg[mtcars$am == 1]
+
+#once again plotting manual vs automatic transmission mpg
+
+boxplot(auto, manu)
 
 #performing t.test for not paired means
 t.tes(auto, manu)
@@ -30,3 +34,9 @@ pairs(mtcars)
 fit1 <- lm(mpg ~ ., data = mtcars)
 
 summary(fit1)
+
+#selecting model with step function
+fit2 <- step(fit1)
+
+summary(fit2)
+
